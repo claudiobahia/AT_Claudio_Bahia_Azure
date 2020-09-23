2b0e8b6252f1242eaa1826c50c37d0d31b7413f8
claudiobahia <claudio.bahia@al.infnet.edu.br>
2020-09-23 15:50:52 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Amigos/Amigo.cs
@@ -0,0 +1,23 @@
+﻿using Domain._Mundo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Domain.Amigos
+{
+    public class Amigo
+    {
+        public int id { get; set; }
+        public string foto { get; set; }
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string email { get; set; }
+        public string telefone { get; set; }
+        public string dataaniversario { get; set; }
+        public List<Amigo> amigos { get; set; } = new List<Amigo>();
+        public Pais pais { get; set; }
+        public Estado estado { get; set; }
+
+    }
+}
---
+++ Domain/´Mundo/Pais.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain._Mundo
+{
+    public class Pais
+    {
+        public int id { get; set; }
+        public int foto { get; set; }
+        public string nome { get; set; }
+        public List<Estado> estados { get; set; } = new List<Estado>();
+
+    }
+}
